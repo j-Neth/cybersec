deac600cc39260181c16780bd160d2263b39d1af
j-Neth <nethwgf@gmail.com>
2026-02-02 09:29:10 +0100

    Modified the file where the log gets written

--- leggo.py
+++ leggo.py
@@ -1,9 +1,10 @@
 import sys
-nomefile = sys.argv[1]
+nomefile = logfile.txt
 try:
     f = open(nomefile, "r", encoding='utf-8')
     righe = f.readlines()
     for riga in righe:
         print(riga, end = '')
 except FileNotFoundError as e:
+
     print(f"[-] errore bloccante {str(e)}")
