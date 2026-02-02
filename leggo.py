f087c79b5aa3b0d32479d0c7e68010e57093c0a7
j-Neth <nethwgf@gmail.com>
2026-02-02 09:36:50 +0100

    deleted import sys beacuse isn't needed

--- leggo.py
+++ leggo.py
@@ -1,4 +1,3 @@
-import sys
 nomefile = logfile.txt
 try:
     f = open(nomefile, "r", encoding='utf-8')
@@ -8,3 +7,4 @@ try:
 except FileNotFoundError as e:
 
     print(f"[-] errore bloccante {str(e)}")
+
